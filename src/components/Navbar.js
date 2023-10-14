7a40106efeb60499dffebc2beede74399f432ea3
falshami2002 <115384875+falshami2002@users.noreply.github.com>
2023-10-13 18:17:14 -0700

    made changes to the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,37 +9,21 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Create Conference</a></li>
+                        <li><a>View Conferences</a></li>
                     </ul>
                 </div>
-                <Link to="/" className="btn btn-ghost normal-case text-xl">Conference Go</Link>
+                <Link to="/" className="btn btn-ghost normal-case text-sm">CONFERENCE GO</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><a>Attend Conference</a></li>
+                    <li><a>Create Conference</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link to="/login" className="btn mr-5">Login</Link>
-                <Link to="/signup" className="btn">Sign Up</Link>
+                <Link to="/login" className="btn btn-ghost normal-case text-sm mr-5">LOGIN</Link>
+                <Link to="/signup" className="btn btn-ghost normal-case text-sm mr-5">SIGN UP</Link>
             </div>
         </div>
     )
