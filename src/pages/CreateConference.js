71b271d253d4e758b531753936d94def7003cecc
Shadan <shadanamini@gmail.com>
2023-11-03 18:16:14 -0700

    Changed Create Conference Form

--- src/pages/CreateConference.js
+++ src/pages/CreateConference.js
@@ -25,65 +25,53 @@ const CreateConferenceForm = () => {
   };
 
   return (
-    <div>
-      <h2>Create Conference</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Conference Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={conferenceData.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="maxAttendees">Max Attendees:</label>
-          <input
-            type="number"
-            id="maxAttendees"
-            name="maxAttendees"
-            value={conferenceData.maxAttendees}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            value={conferenceData.city}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="state">State:</label>
-          <input
-            type="text"
-            id="state"
-            name="state"
-            value={conferenceData.state}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="country">Country:</label>
-          <input
-            type="text"
-            id="country"
-            name="country"
-            value={conferenceData.country}
-            onChange={handleInputChange}
-          />
-        </div>
-        {/* Add input fields for presentations (e.g., an array of presentation names) */}
-        {/* For presentations, you can add more input fields and use state to manage the list */}
-        <div>
-          <button type="submit">Create Conference</button>
-        </div>
-      </form>
+    <div className="attend-conference-container overflow-y-hidden bg-base-200">
+      <div className="black-and-white-form">
+        <h2 div className="text-center font-bold">Create Conference</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="firstName">Conference Name</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="conferenceDate">Conference Date</label>
+            <input
+
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName">Conference Location</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName">Number of Attendees</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName">Conference Description</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group rounded-button-container font-bold">
+            <button type="submit">Create!</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
