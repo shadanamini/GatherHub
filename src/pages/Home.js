624ebee7c0b9e543cc6bc2cd12e321aca79ef387
Shadan <shadanamini@gmail.com>
2023-11-16 21:16:01 -0800

    reverted to no check mark functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,30 +4,19 @@ import Navbar from "../components/Navbar";
 import { useConferenceContext } from "./ConferenceContext";
 
 const Home = () => {
-  const { createdConferences, attendedConferences, addAttendedConference } = useConferenceContext();
+  const { createdConferences } = useConferenceContext();
 
-    const renderConferenceBubbles = (conferences) => {
+  const renderConferenceDetails = (conferences) => {
     return conferences.map((conference, index) => (
-        <div key={index} className="flex mb-4 bg-black rounded-lg border border-white p-4 relative">
+      <div key={index} className="flex mb-4 bg-black rounded-lg border border-white p-4">
         <div className="text-gray-300">
-            <p>Name: {conference.Name}</p>
-            <p>Date: {conference.Date}</p>
-            <p>Location: {conference.Location}</p>
-            <p>Attendees: {conference.Attendees}</p>
+          <p>Name: {conference.Name}</p>
+          <p>Date: {conference.Date}</p>
+          <p>Location: {conference.Location}</p>
+          <p>Attendees: {conference.Attendees}</p>
         </div>
-        <button
-            className="absolute top-0 right-0 m-2 bg-white text-black font-bold py-2 px-4 rounded-full"
-            onClick={() => handleAttendClick(conference)}
-        >
-            ✔
-        </button>
-        </div>
+      </div>
     ));
-    };
-
-  const handleAttendClick = (conference) => {
-    // Add the selected conference to the "Attending Conferences" list
-    addAttendedConference(conference);
   };
 
   return (
@@ -38,7 +27,7 @@ const Home = () => {
         <div className="h-[90%] w-[90%] bg-black rounded-xl p-4">
           <h2 className="text-white text-lg font-bold mb-4">Created Conferences</h2>
           {createdConferences && createdConferences.length > 0 ? (
-            renderConferenceBubbles(createdConferences)
+            renderConferenceDetails(createdConferences)
           ) : (
             <p>No created conferences yet.</p>
           )}
@@ -47,11 +36,7 @@ const Home = () => {
         {/* Right Side - Attending Conferences */}
         <div className="h-[90%] w-[90%] bg-black rounded-xl p-4">
           <h2 className="text-white text-lg font-bold mb-4">Attending Conferences</h2>
-          {attendedConferences && attendedConferences.length > 0 ? (
-            renderConferenceBubbles(attendedConferences)
-          ) : (
-            <p>No attended conferences yet.</p>
-          )}
+          {/* Render attending conference details if needed */}
         </div>
       </div>
     </div>
